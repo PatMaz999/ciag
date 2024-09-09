f14b4b2a2eca923faf1cc92ca8903f639dca1b20
Patryk Mazurek <Patryk.m3456@gmail.com>
2024-09-09 14:03:34 +0200

    poprawiony blad liczenia od konca a nie poczatku

--- main.cpp
+++ main.cpp
@@ -20,14 +20,13 @@ private:
     int rozmiar; //liczymy od 0
     int stopien;
     //char znak; //dla konstruktora ze stringiem
-    void wypisz_wartosc(ostream& strumien, const double* liczba, const int sto/*, bool ostatni = false*/);
-    void wypisz_stopien(ostream& strumien, const int liczba/*, bool ostatni = false*/);
-    //void znak(ostream& strumien, const double* liczba, bool ostatni = false);
+    void wypisz_wartosc(ostream& strumien, const double* liczba, const int sto);
+    void wypisz_stopien(ostream& strumien, const int liczba);
 protected:
     double* wzor;
 public:
-    ciag() : rozmiar(0), stopien(0)/*, znak('+')*/, wzor(new double[1]) { wzor[0] = 0; };
-    ciag(initializer_list<double> lista/*, char zn = '+'*/); //znak bardziej jako dodatek
+    ciag() : rozmiar(0), stopien(0), wzor(new double[1]) { wzor[0] = 0; };
+    ciag(initializer_list<double> lista);
     ~ciag() { delete[] wzor; };
     friend ostream& operator<<(ostream& strumien, ciag & c); //ciag const
     //void wypisz();
@@ -36,7 +35,7 @@ public:
 
 //////////////////*konstruktory*/
 
-ciag::ciag(initializer_list<double> lista/*, char zn*/): /*znak(zn),*/ rozmiar(lista.size() - 1){
+ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){
     wzor = new double[lista.size()];
     for (int i = 0; i < lista.size(); ++i) {
         wzor[i] = *(lista.begin() + i);
@@ -58,51 +57,48 @@ ostream& operator<<(ostream& strumien, ciag& c) { //ciag const// //brag referenc
         return strumien;
     }
     int ile_zer = 0;
-    for (int i = c.rozmiar; i >= 0; i--) {
+    for (int i = 0; i <= c.rozmiar; ++i) {
         if (c.wzor[i] == 0)
             ++ile_zer;
         else
             break;
     }
-    for (int i = 0; i <= c.rozmiar; ++i) {
-        if ((i + ile_zer) == c.rozmiar) {
-            c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - i/*, true*/);
+    for (int i = ile_zer; i <= c.rozmiar; ++i) {
+        if (i == c.rozmiar) {
+            c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - i); //ostatnie wywo³anie (bez dodawania + na koñcu)
             break;
         }
         c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - i);
-        if (c.wzor[i+1] > 0 /*&& ostatni == false*/)
+        if (c.wzor[i + 1] > 0) {
             strumien << " + ";
-        else if(c.wzor[i + 1] != 0)
+        }
+        else if (c.wzor[i + 1] != 0) {
             strumien << " ";
+        }
     }
     return strumien;
 }
 
-void ciag::wypisz_wartosc(ostream& strumien, const double* liczba, const int sto/*, bool ostatni*/) {
+void ciag::wypisz_wartosc(ostream& strumien, const double* liczba, const int sto) {
     if (*liczba == -1) //je¿eli liczba wynosi -1 dodaj na pocz¹tku znak '-' i wykonaj operacje dla liczby 1
         strumien << "- ";
     if (*liczba == 1 || *liczba == -1) {
         if(sto != 0)
-            wypisz_stopien(strumien, sto/*, ostatni*/);
+            wypisz_stopien(strumien, sto);
         else
             strumien << *liczba;
     }
     else if (*liczba != 1 && liczba != 0){
         if (*liczba > 0) {
             strumien << *liczba;
-            wypisz_stopien(strumien, sto/*, ostatni*/);
+            wypisz_stopien(strumien, sto);
         }
         else if (*liczba < 0) {
             strumien << "- " << abs(*liczba);
-            wypisz_stopien(strumien, sto/*, ostatni*/);
+            wypisz_stopien(strumien, sto);
         }
-        //wypisz_stopien(strumien, sto/*, ostatni*/); //zamiast dwóch oddzielnych instrukcji, z jakiegoœ powodu nie dzia³a
+        //wypisz_stopien(strumien, sto); //zamiast dwóch oddzielnych instrukcji, z jakiegoœ powodu nie dzia³a
     }
-
-    //if (*liczba > 0 && ostatni == false)
-    //    strumien << " + ";
-    //else
-    //    strumien << " ";
 }
 
 void ciag::wypisz_stopien(ostream& strumien, const int sto/*, bool ostatni*/) {
@@ -146,9 +142,8 @@ class ciag_geometryczny {
 /*******************************main*************************************************/
 int main()
 {
-    ciag cba{0,0,-4,1,-2,0,0,-3,0,1,0};
-    //ciag abc{ {0,2,0,1,0}, '-' };
-    //ciag cba{0,1,0,1,0};
+    ciag cba{0,0,0,-4,1,-2,0,0,-3,0,1,0,0,0};
+    //ciag cba{3,0,0,1};
     cout << cba << "\t\t"/* << cba << endl*/;
     cout << cba.suma(2);
     //ciag abcd{ {1} };
