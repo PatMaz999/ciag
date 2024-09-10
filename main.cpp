b85d9b6a8a0a8a3af866809ed3de5ac564ef508e
Patryk Mazurek <Patryk.m3456@gmail.com>
2024-09-10 13:56:56 +0200

    konstruktor dla typu string(niedokonczony)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <initializer_list>
 #include <cmath>
+#include <string>
 
 //popraw przypadek pustej listy inizjalizacyjnej (konstruktor)
 //mechanizm kopiowania i przenoszenia
 //dodaj funkcje wypisz();
+//popraw niepoprawny rozmiar w konstruktorze dla listy inicjalizacyjnej (tymczasowo równie¿ dla string) i operatorze<<
+//konstruktor dla string dzia³a tylko dla najmniejszego elementu stopnia 2 lub wy¿szego
 
 using namespace std;
 
@@ -17,7 +20,7 @@ ostream& operator<<(ostream& strumien, ciag& c); //ciag const
 
 class ciag {
 private:
-    int rozmiar; //liczymy od 0
+    int rozmiar; //liczymy od 0 // o 1 mniejszy ni¿ powinien
     int stopien;
     //char znak; //dla konstruktora ze stringiem
     void wypisz_wartosc(ostream& strumien, const double* liczba, const int sto);
@@ -27,6 +30,7 @@ protected:
 public:
     ciag() : rozmiar(0), stopien(0), wzor(new double[1]) { wzor[0] = 0; };
     ciag(initializer_list<double> lista);
+    ciag(string napis);
     ~ciag() { delete[] wzor; };
     friend ostream& operator<<(ostream& strumien, ciag & c); //ciag const
     //void wypisz();
@@ -35,7 +39,7 @@ public:
 
 //////////////////*konstruktory*/
 
-ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){
+ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){ //rozmiar + 1 ???
     wzor = new double[lista.size()];
     for (int i = 0; i < lista.size(); ++i) {
         wzor[i] = *(lista.begin() + i);
@@ -46,6 +50,42 @@ ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){
             --stopien;
         else
             break;
+    }
+}
+
+ciag::ciag(string napis){
+    string liczba = "";
+    string liczba_stopien = "";
+    int obecny_stopien = 0;
+    double obecna_liczba = 0;
+    bool pierwszy_stopien = false;
+    for (int i = 0; i < napis.size(); ++i) {
+        if (isdigit(napis[i]) || napis[i] == '.') {
+            for (; i < napis.size() && isdigit(napis[i]) && napis[i] != '.'; ++i)
+                liczba += napis[i];
+            obecna_liczba = stof(liczba);
+            liczba = "";
+        }
+        else if (isalpha(napis[i])) {
+            continue;
+        }
+        else if (napis[i] == '^') {
+            ++i;
+            for (; i < napis.size() && isdigit(napis[i]); ++i)
+                liczba_stopien += napis[i];
+            obecny_stopien = stoi(liczba_stopien);
+            liczba_stopien = "";
+            if (pierwszy_stopien == false) {
+                this->rozmiar = this->stopien = obecny_stopien; // konstrukcja obiektu
+                this->wzor = new double[rozmiar + 1]; // alokacja pamieci // rozmiar jest o 1 mniejszy ni¿ powinien
+                for (int j = 0; j < rozmiar + 1; ++j) // przypisz 0 do tablicy // rozmiar jest o 1 mniejszy ni¿ powinien
+                    this->wzor[j] = 0;
+                pierwszy_stopien = true;
+            }
+            wzor[this->stopien - obecny_stopien] = obecna_liczba;
+            obecna_liczba = 0;  // prawdopodobnie niepotrzebne
+            obecny_stopien = 0; // prawdopodobnie niepotrzebne
+        }
     }
 }
 
@@ -142,8 +182,10 @@ class ciag_geometryczny {
 /*******************************main*************************************************/
 int main()
 {
-    ciag cba{0,0,0,-4,1,-2,0,0,-3,0,1,0,0,0};
+    //ciag cba{0,0,0,-4,1,-2,0,0,-3,0,1,0,0,0};
     //ciag cba{3,0,0,1};
+    ciag cba{"2n^3 + 2n^2"};
+    //ciag cba{2,0,0};
     cout << cba << "\t\t"/* << cba << endl*/;
     cout << cba.suma(2);
     //ciag abcd{ {1} };
