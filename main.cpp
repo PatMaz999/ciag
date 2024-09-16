b98c389bbb6caa48dec886d7ba0ebda9140763ff
Patryk Mazurek <Patryk.m3456@gmail.com>
2024-09-16 17:15:36 +0200

    konstruktor string dla stopnia 0 i 1

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 //popraw przypadek pustej listy inizjalizacyjnej (konstruktor)
 //mechanizm kopiowania i przenoszenia
 //dodaj funkcje wypisz();
-//popraw niepoprawny rozmiar w konstruktorze dla listy inicjalizacyjnej (tymczasowo równie¿ dla string) i operatorze<<
-//konstruktor dla string dzia³a tylko dla najmniejszego elementu stopnia 2 lub wy¿szego
+//konstruktor dla string dzia³a tylko dla liczb dodatnich i nie obejmuje pomijania liczby 1 (np n^3 + 2)
+//konstruktor dla string nie uwzglêdnia przypadku postawienie kilku kropek
 
 using namespace std;
 
@@ -20,7 +20,7 @@ ostream& operator<<(ostream& strumien, ciag& c); //ciag const
 
 class ciag {
 private:
-    int rozmiar; //liczymy od 0 // o 1 mniejszy ni¿ powinien
+    int rozmiar; //liczymy od 1
     int stopien;
     //char znak; //dla konstruktora ze stringiem
     void wypisz_wartosc(ostream& strumien, const double* liczba, const int sto);
@@ -39,12 +39,12 @@ public:
 
 //////////////////*konstruktory*/
 
-ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){ //rozmiar + 1 ???
-    wzor = new double[lista.size()];
+ciag::ciag(initializer_list<double> lista): rozmiar(lista.size()){
+    wzor = new double[rozmiar];
     for (int i = 0; i < lista.size(); ++i) {
         wzor[i] = *(lista.begin() + i);
     }
-    stopien = rozmiar;
+    stopien = rozmiar - 1;
     for (int i = 0; i < lista.size(); ++i) {
         if (wzor[i] == 0)
             --stopien;
@@ -52,41 +52,59 @@ ciag::ciag(initializer_list<double> lista): rozmiar(lista.size() - 1){ //rozmiar
             break;
     }
 }
+// do zrobienia
+// brak liczby (np n^2 + n)
+// liczba nie jest podnoszona do potegi (proste przypadki)
 
 ciag::ciag(string napis){
     string liczba = "";
     string liczba_stopien = "";
     int obecny_stopien = 0;
-    double obecna_liczba = 0;
+    double obecna_liczba = 0; //domyslnie 1 ???
     bool pierwszy_stopien = false;
+    bool bylo_n = false;
     for (int i = 0; i < napis.size(); ++i) {
         if (isdigit(napis[i]) || napis[i] == '.') {
             for (; i < napis.size() && isdigit(napis[i]) && napis[i] != '.'; ++i)
                 liczba += napis[i];
+            --i; // jezeli przerywamy sprawdzanie to jestesmy o jeden indeks za daleko
             obecna_liczba = stof(liczba);
             liczba = "";
         }
         else if (isalpha(napis[i])) {
+            bylo_n = true; //przed podnienieniem do potegi wystepuje n
             continue;
         }
-        else if (napis[i] == '^') {
+        else if (napis[i] == '^' && bylo_n) {
             ++i;
             for (; i < napis.size() && isdigit(napis[i]); ++i)
                 liczba_stopien += napis[i];
+            //--i //nie ma --i bo kolejne jest ' '
             obecny_stopien = stoi(liczba_stopien);
             liczba_stopien = "";
             if (pierwszy_stopien == false) {
-                this->rozmiar = this->stopien = obecny_stopien; // konstrukcja obiektu
-                this->wzor = new double[rozmiar + 1]; // alokacja pamieci // rozmiar jest o 1 mniejszy ni¿ powinien
-                for (int j = 0; j < rozmiar + 1; ++j) // przypisz 0 do tablicy // rozmiar jest o 1 mniejszy ni¿ powinien
+                this->rozmiar = obecny_stopien + 1;
+                this->stopien = obecny_stopien; // konstrukcja obiektu
+                this->wzor = new double[rozmiar]; // alokacja pamieci
+                for (int j = 0; j < rozmiar; ++j) // przypisz 0 do tablicy
                     this->wzor[j] = 0;
                 pierwszy_stopien = true;
             }
             wzor[this->stopien - obecny_stopien] = obecna_liczba;
-            obecna_liczba = 0;  // prawdopodobnie niepotrzebne
-            obecny_stopien = 0; // prawdopodobnie niepotrzebne
+            obecna_liczba = 0; //potrzebne bo jak nie ma liczby to w ostatnim przypisaniu jest domyœlnie 0
+            obecny_stopien = 0;
+            bylo_n = false; //podnieslismy do potegi wiec sprawdzamy kolejne
+        }
+        else if (bylo_n && napis[i] != '^') {
+            wzor[this->stopien - 1] = obecna_liczba; //przedostatni element
+            obecna_liczba = 0;
+            bylo_n = false;
         }
     }
+    if (bylo_n)
+        wzor[this->stopien - 1] = obecna_liczba;
+    else
+        wzor[this->stopien] = obecna_liczba;
 }
 
 ////////////////////*operator<<*/
@@ -97,18 +115,18 @@ ostream& operator<<(ostream& strumien, ciag& c) { //ciag const// //brag referenc
         return strumien;
     }
     int ile_zer = 0;
-    for (int i = 0; i <= c.rozmiar; ++i) {
+    for (int i = 0; i < c.rozmiar; ++i) {
         if (c.wzor[i] == 0)
             ++ile_zer;
         else
             break;
     }
-    for (int i = ile_zer; i <= c.rozmiar; ++i) {
-        if (i == c.rozmiar) {
-            c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - i); //ostatnie wywo³anie (bez dodawania + na koñcu)
+    for (int i = ile_zer; i < c.rozmiar; ++i) {
+        if (i == c.rozmiar - 1) {       //rozmiar - 1 bo indeksy liczymy od 0
+            c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - 1 - i); //ostatnie wywo³anie (bez dodawania + na koñcu)
             break;
         }
-        c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - i);
+        c.wypisz_wartosc(strumien, c.wzor + i, c.rozmiar - 1 - i);
         if (c.wzor[i + 1] > 0) {
             strumien << " + ";
         }
@@ -141,7 +159,7 @@ void ciag::wypisz_wartosc(ostream& strumien, const double* liczba, const int sto
     }
 }
 
-void ciag::wypisz_stopien(ostream& strumien, const int sto/*, bool ostatni*/) {
+void ciag::wypisz_stopien(ostream& strumien, const int sto) {
     if (sto > 1) {
         strumien << "n^" << sto;
         //if (ostatni == false)
@@ -154,17 +172,12 @@ void ciag::wypisz_stopien(ostream& strumien, const int sto/*, bool ostatni*/) {
     }
 }
 
-//void ciag::znak(ostream& strumien, const double* liczba, bool ostatni) {
-//    if (*liczba > 0 && ostatni == false)
-//        strumien << " + ";
-//}
-
 ////////////////////*suma*/
 
 double ciag::suma(int n){
     double sum = 0.0;
     int sto = stopien; //nie modyfikuj stopnia
-    for (int i = rozmiar - stopien; i < rozmiar; ++i) {
+    for (int i = rozmiar - 1 - stopien; i <= rozmiar; ++i) { // - 1 bo rozmiar liczymy od 1 a indeksy od 0, <= a nie < z tego samego powodu
         sum += wzor[i] * pow(n, sto--);
     }
     return sum;
@@ -184,7 +197,7 @@ int main()
 {
     //ciag cba{0,0,0,-4,1,-2,0,0,-3,0,1,0,0,0};
     //ciag cba{3,0,0,1};
-    ciag cba{"2n^3 + 2n^2"};
+    ciag cba{"2n^3 + 2n^2 + 3n + 2"}; // dzia³a te¿ bez spacji
     //ciag cba{2,0,0};
     cout << cba << "\t\t"/* << cba << endl*/;
     cout << cba.suma(2);
